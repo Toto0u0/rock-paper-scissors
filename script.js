0314ef7ff2abd1e5e8f3b639ac99d607ecb42c99
Toto0o0 <189169972+Toto0o0@users.noreply.github.com>
2025-05-17 18:58:24 -0700

    feat: game stopping if the player or computer reach a score of 3

--- script.js
+++ script.js
@@ -1,61 +1,83 @@
 let humanScore = 0;
 let computerScore = 0;
+let gameCount = 0;
+
 
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3);
+    const cpu = Math.floor(Math.random() * 3);
+    return cpu;
+}
+
+function getHumanChoice() {
+    const userInput = parseInt(prompt("0 for rock, 1 for paper or 2 for scissors?: "));
+    human = userInput;
+    return human;
     }
-    const rndInt = getComputerChoice();
 
-
-
-let userInput = parseInt(prompt("0 for rock, 1 for paper or 2 for scissors?: "));
-function getHumanChoice(userInput) {
-    return userInput;
-    }
-
-console.log(`user choice ${userInput}`);
-console.log(`cpu choice ${rndInt}`);
+// console.log(`user choice ${getHumanChoice()}`);
+// console.log(`cpu choice ${getComputerChoice()}`);
 
 function playRound(humanChoice, computerChoice) {
-    if (userInput === 0 && rndInt === 0) {
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+
+    humanSelection;
+    computerSelection;
+
+    if (humanSelection === 0 && computerSelection === 0) {
         console.log("tie");
-    } else if (userInput === 1 && rndInt === 0) {
+        gameCount +=1;
+    } else if (humanSelection === 1 && computerSelection === 0) {
         console.log("you win");
         humanScore += 1;
-    } else if (userInput === 0 && rndInt === 1) {
+        gameCount +=1;
+    } else if (humanSelection === 0 && computerSelection === 1) {
         console.log("you lose");
         computerScore +=1;
-    } else if (userInput === 1 && rndInt === 1) {
+        gameCount +=1;
+    } else if (humanSelection === 1 && computerSelection === 1) {
         console.log("tie");
-    } else if (userInput === 2 && rndInt === 1) {
+        gameCount +=1;
+    } else if (humanSelection === 2 && computerSelection === 1) {
         console.log("you win");
         humanScore += 1;
-    } else if (userInput === 0 && rndInt === 2) {
+        gameCount +=1;
+    } else if (humanSelection === 0 && computerSelection === 2) {
         console.log("you win");
         humanScore += 1;
-    } else if (userInput === 1 && rndInt === 2) {
+        gameCount +=1;
+    } else if (humanSelection === 1 && computerSelection === 2) {
         console.log("you lose");
         computerScore +=1;
-    } else if (userInput === 2 && rndInt === 2) {
+        gameCount +=1;
+    } else if (humanSelection === 2 && computerSelection === 2) {
         console.log("tie");
-    } else if (userInput === 2 && rndInt == 0) {
+        gameCount +=1;
+    } else if (humanSelection === 2 && computerSelection == 0) {
         console.log("you lose");
         computerScore +=1;
+        gameCount +=1;
     }
-    console.log(`Score is ${humanScore} to ${computerScore}`);
-    getComputerChoice();
-    userInput = parseInt(prompt("0 for rock, 1 for paper or 2 for scissors?: "));
-    console.log(`user choice ${userInput}`);
-    console.log(`cpu choice ${rndInt}`);
+
+    console.log("                             ");
+    console.log(`user choice ${humanSelection}`);
+    console.log(`cpu choice ${computerSelection}`);
+    console.log("                               ");
+    console.log(`Game ${gameCount} score is: ${humanScore} to ${computerScore}`);
 
 }
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+// playRound(humanSelection, computerSelection);
+
+// function playGame() {
+//     for (var i = 1; i < 6; i++) playRound();
+// }
 
 function playGame() {
-    for (var i = 1; i < 5; i++) playRound();
+    while (humanScore < 3 && computerScore < 3) 
+        playRound();
 }
 
 playGame();
